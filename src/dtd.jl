fc5b3f6fb7965b3bed88fbbd18eaf24a207e16ef
Josh Day <emailjoshday@gmail.com>
2023-04-28 16:30:46 -0400

    DTD parsing done-ish

--- src/dtd.jl
+++ src/dtd.jl
@@ -1,3 +1,14 @@
+#-----------------------------------------------------------------------------# position_after
+function position_after(needle::Vector{UInt8}, haystack::Vector{UInt8}, i)
+    x = findnext(needle, haystack, i)
+    isnothing(x) ? nothing : x[end] + 1
+end
+
+position_after(needle::String, haystack::Vector{UInt8}, i) = position_after(Vector{UInt8}(needle), haystack, i)
+
+
+
+
 #-----------------------------------------------------------------------------# DeclaredElement
 struct DeclaredElement
     name::String
@@ -11,7 +22,7 @@ end
 Base.show(io::IO, o::DeclaredElement) = print(io, "<!ELEMENT ", o.name, " ", o.content, ">")
 
 function get_declared_elements(data::Vector{UInt8})
-    i = findnext(Vector{UInt8}("<!ELEMENT"), data, 1)[end]
+    i = position_after("<!ELEMENT", data, 1)
     out = DeclaredElement[]
     while !isnothing(i)
         name, i = get_name(data, i + 1)
@@ -23,8 +34,7 @@ function get_declared_elements(data::Vector{UInt8})
             content, i = get_name(data, i)
         end
         push!(out, DeclaredElement(name, content))
-        fn = findnext(Vector{UInt8}("<!ELEMENT"), data, i)
-        i = isnothing(fn) ? nothing : fn[end]
+        i = position_after("<!ELEMENT", data, i)
     end
     return out
 end
@@ -38,19 +48,56 @@ struct DeclaredAttribute
 end
 Base.show(io::IO, o::DeclaredAttribute) = print(io, "<!ATTLIST ", o.element_name, " ", o.attribute_name, " ", o.attribute_type, " ", o.attribute_value, ">")
 
+
 function get_declared_attributes(data)
-    []
+    i = position_after("<!ATTLIST", data, 1)
+    out = DeclaredAttribute[]
+    while !isnothing(i)
+        element_name, i = get_name(data, i)
+        attribute_name, i = get_name(data, i)
+        i = findnext(!isspace, data, i)
+        attribute_type = if data[i] == UInt('(')
+            j = findnext(==(UInt8(')')), data, i)
+            String(data[i:j])
+            i = j + 1
+        else
+            nm, i = get_name(data, i)
+            nm
+        end
+        i = findnext(!isspace, data, i)
+        is_hash = data[i] == UInt8('#')
+        val, i = get_name(data, i)
+        attribute_value = is_hash ? '#' * val : val
+        push!(out, DeclaredAttribute(element_name, attribute_name, attribute_type, attribute_value))
+        i = position_after("<!ATTLIST", data, i)
+    end
+    return out
 end
 
 #-----------------------------------------------------------------------------# DeclaredEntity
 struct DeclaredEntity
     name::String
+    external::Bool
     value::String
 end
-Base.show(io::IO, o::DeclaredEntity) = print(io, "<!ENTITY ", o.name, " ", o.value, ">")
+function Base.show(io::IO, o::DeclaredEntity)
+    print(io, "<!ENTITY ", o.name, " ", o.external ? "SYSTEM" : "", repr(o.value), ">")
+end
 
 function get_declared_entities(data)
-    []
+    i = position_after("<!ENTITY", data, 1)
+    out = DeclaredEntity[]
+    while !isnothing(i)
+        name, i = get_name(data, i)
+        value, i = get_name(data, i)
+        external = value == "SYSTEM"
+        if external
+            value, i = get_name(data, i)
+        end
+        push!(out, DeclaredEntity(name, external, value))
+        i = position_after("<!ENTITY", data, i)
+    end
+    return out
 end
 
 #-----------------------------------------------------------------------------# DTDBody
@@ -62,19 +109,19 @@ struct DTDBody
 end
 
 function Base.show(io::IO, o::DTDBody)
-    println(io, "DTDBody(root=\"", o.root)
-    println(io, "  • DeclaredElements")
-    foreach(x -> println(io, "    ", x), o.elements)
-    println(io, "  • DeclaredAttributes")
-    println(io, "  • DeclaredEntities")
+    printstyled(io, "DTDBody(root=\"", o.root, "\")\n", color=:light_cyan)
+    printstyled(io, "   DeclaredElements (", length(o.elements), ")\n", color=:light_green)
+    foreach(x -> println(io, "        ", x), o.elements)
+    printstyled(io, "    DeclaredAttributes (", length(o.attributes), ")\n", color=:light_green)
+    foreach(x -> println(io, "        ", x), o.attributes)
+    printstyled(io, "    DeclaredEntities (", length(o.entities), ")\n", color=:light_green)
+    foreach(x -> println(io, "        ", x), o.entities)
 end
 
 
 
 function DTDBody(data::Vector{UInt8})
-    start = "<!DOCTYPE"
-    data[1:length(start)] == Vector{UInt8}(start) || error("DTD must start with `<!DOCTYPE`.")
-    i = length(start) + 1
+    i = position_after("<!DOCTYPE", data, 1)
     root, i = get_name(data, i)
 
     i = findnext(==(UInt8('[')), data, i)
